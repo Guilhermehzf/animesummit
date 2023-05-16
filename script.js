1468afe679ed8adbb877c18196689ad16aca4b5b
Guilherme Paes <110517542+Guilhermehzf@users.noreply.github.com>
2023-05-16 11:56:46 -0300

    site anime summit

---
+++ script.js
@@ -0,0 +1,20 @@
+    // Obtém referência ao formulário de login
+    var loginForm = document.getElementById('login-form');
+
+    // Adiciona um ouvinte de evento para o envio do formulário
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Impede o envio do formulário
+
+      // Obtém os valores inseridos pelo usuário
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+
+      // Verifica as credenciais do usuário
+      if (username === 'guilherme' && password === 'gatitcha1') {
+        // Redireciona para o link desejado
+        window.location.href = 'https://www.linkedin.com/?trk=public_post_nav-header-logo';
+      } else {
+        // Credenciais inválidas, exibe uma mensagem de erro ou realiza outra ação
+        alert('Credenciais inválidas!');
+      }
+    });
