895f7d085533799e73d18e6e35060c2a358e8176
Guilherme Paes <110517542+Guilhermehzf@users.noreply.github.com>
2023-05-24 08:52:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-    // Obtém referência ao formulário de login
-    var loginForm = document.getElementById('login-form');
-    function togglePasswordVisibility() {
-      var passwordInput = document.getElementById("senha");
+// Obtém referência ao formulário de login
+var loginForm = document.getElementById('login-form');
 
-      if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-      } else {
-        passwordInput.type = "password";
-      }
-    }
-    // Adiciona um ouvinte de evento para o envio do formulário
-    loginForm.addEventListener('submit', function(event) {
-      event.preventDefault(); // Impede o envio do formulário
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+  var passwordToggle = document.querySelector('.password-toggle');
 
-      // Obtém os valores inseridos pelo usuário
-      var username = document.getElementById('username').value;
-      var password = document.getElementById('password').value;
-        
-      // Verifica as credenciais do usuário
-      if (username === 'guilherme' && password === 'gatitcha1') {
-        // Redireciona para o link desejado
-        window.location.href = 'https://www.linkedin.com/?trk=public_post_nav-header-logo';
-      } else {
-        // Credenciais inválidas, exibe uma mensagem de erro ou realiza outra ação
-        alert('Credenciais inválidas!');
-      }
-    });
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordToggle.classList.add('show-password');
+  } else {
+    passwordInput.type = "password";
+    passwordToggle.classList.remove('show-password');
+  }
+}
+
+// Adiciona um ouvinte de evento para o envio do formulário
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Impede o envio do formulário
+
+  // Obtém os valores inseridos pelo usuário
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Verifica as credenciais do usuário
+  if (username === 'guilherme' && password === 'gatitcha1') {
+    // Redireciona para o link desejado
+    window.location.href = 'https://www.linkedin.com/?trk=public_post_nav-header-logo';
+  } else {
+    // Credenciais inválidas, exibe uma mensagem de erro ou realiza outra ação
+    alert('Credenciais inválidas!');
+  }
+});
